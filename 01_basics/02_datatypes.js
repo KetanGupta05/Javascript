6ebb85c1a2ce79b8d8dc217cbb8cbaa3229b4665
KetanGupta05 <152555419+KetanGupta05@users.noreply.github.com>
2024-01-12 19:18:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+let name ="ketan"
+let age =18
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint
+// string=> ""
+// null=> standalone value
+// undefined => value assign nhi hui h 
+// symbol => unique
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
