eb8d3a8aee036d42b138bfba1c37d63988a3c4ac
KetanGupta05 <152555419+KetanGupta05@users.noreply.github.com>
2024-01-12 19:39:41 +0000

    conversions

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,16 @@
+let score = 33
+
+console.log(typeof score);
+
+let valueInNumber= String(score)
+
+console.log(typeof valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn=1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsloggedIn);
